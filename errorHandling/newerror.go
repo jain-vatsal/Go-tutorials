fab091b210896db06f8abc4edbe9853a03a23772
vatsal <vatsaleduc@gmail.com>
2024-04-07 10:41:03 +0530

    updated errors

---
+++ errorHandling/newerror.go
@@ -0,0 +1,9 @@
+// errors.New
+package main
+
+// func divide(x, y float64) (float64, error) {
+// 	if y == 0 {
+// 		return 0.0, errors.New("can't divide by zero")
+// 	}
+// 	return x / y, nil
+// }
